4f8482e570715a9762ceea01567388a4555ef948
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-05 17:41:22 +0530

    Learned About TomCat Servlet

---
+++ SimpleJavaWebServer/src/test/java/com/example/SimpleJavaWebServer/SimpleJavaWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleJavaWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
