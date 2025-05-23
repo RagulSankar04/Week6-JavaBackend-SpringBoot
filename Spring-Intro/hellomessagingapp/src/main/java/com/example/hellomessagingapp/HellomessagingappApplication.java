6937eb810aa102d9fbbc708702c16adbacaf55c9
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-23 16:03:17 +0530

    Code Corrections

--- Spring-Intro/hellomessagingapp/src/main/java/com/example/hellomessagingapp/HellomessagingappApplication.java
+++ Spring-Intro/hellomessagingapp/src/main/java/com/example/hellomessagingapp/HellomessagingappApplication.java
@@ -11,35 +11,3 @@ public class HellomessagingappApplication {
 	}
 
 }
-
-/*
-UC 1:
-URL: http://localhost:8080/hello
-Method: GET
-Response: Hello from BridgeLabz
-
-UC 2:
-URL: http://localhost:8080/hello/query?name=Sanjay
-Method: GET
-Response: Hello Sanjay from BridgeLabz
-
-UC 3:
-URL: http://localhost:8080/hello/param/Sanjay
-Method: GET
-Response: Hello Sanjay from BridgeLabz
-
-UC 4:
-URL: http://localhost:8080/hello/post
-Method: POST
-Request Body (JSON):
-{
-  "firstName": "Sanjay",
-  "lastName": "Praveen"
-}
-Response: Hello Sanjay Praveen from BridgeLabz
-
-UC 5:
-URL: http://localhost:8080/hello/put/Sanjay?lastName=Praveen
-Method: PUT
-Response: Hello Sanjay Praveen from BridgeLabz
-*/
