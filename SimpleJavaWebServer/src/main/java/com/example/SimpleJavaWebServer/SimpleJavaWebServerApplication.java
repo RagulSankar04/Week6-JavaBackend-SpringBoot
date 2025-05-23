4acd9d84b554820e5983196df2d1ceec41dbcb28
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-23 15:54:56 +0530

    Removed README.md File

---
+++ SimpleJavaWebServer/src/main/java/com/example/SimpleJavaWebServer/SimpleJavaWebServerApplication.java
@@ -0,0 +1,11 @@
+package com.example.SimpleJavaWebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SimpleJavaWebServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaWebServerApplication.class, args);
+    }
+}
